5a8ed32a98b63ff6fc05d3f136cc133e7672719d
Callum Breen <callumbreen@googlemail.com>
2019-09-11 20:33:48 +0100

    Add CreateUser validation

---
+++ src/NoteTaker.API/ViewModelValidators/CreateUserValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using NoteTaker.API.ViewModels;
+
+namespace NoteTaker.API.ViewModelValidators
+{
+    public class CreateUserValidator : AbstractValidator<CreateUser>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(x => x.Username).NotEmpty();
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.Password).MinimumLength(6).MaximumLength(60).NotNull();
+        }
+    }
+}
---
+++ test/NoteTaker.API.Tests/ViewModelValidators/CreateUserValidatorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation.TestHelper;
+using NoteTaker.API.ViewModelValidators;
+using Xunit;
+
+namespace NoteTaker.API.Tests.ViewModelValidators
+{
+    public class CreateUserValidatorTests
+    {
+        private CreateUserValidator validator;
+
+        public CreateUserValidatorTests()
+        {
+            this.validator = new CreateUserValidator();
+        }
+
+        [Fact]
+        public void Should_Error_When_Username_Is_Null()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.Username, null as string);
+        }
+
+        [Fact]
+        public void Should_Error_When_FirstName_Is_Null()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.FirstName, null as string);
+        }
+
+        [Fact]
+        public void Should_Error_When_LastName_Is_Null()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.LastName, null as string);
+        }
+
+        [Fact]
+        public void Should_Error_When_Password_Is_Null()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.Password, null as string);
+        }
+
+        [Fact]
+        public void Should_Error_When_Password_Is_Too_Short()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.Password, new string('\t', 5));
+        }
+
+        [Fact]
+        public void Should_Error_When_Password_Is_Too_Long()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.Password, new string('\t', 61));
+        }
+    }
+}
