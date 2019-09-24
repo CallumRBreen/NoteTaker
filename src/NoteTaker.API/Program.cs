e062f6c8ad0a39f4b32a45d3a954ccee0b6e413c
Callum Breen <callumbreen@googlemail.com>
2019-09-24 21:11:04 +0100

    Run migrations on startup

--- src/NoteTaker.API/Program.cs
+++ src/NoteTaker.API/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using NoteTaker.DAL;
 
@@ -7,13 +9,17 @@ namespace NoteTaker.API
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateWebHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
+
+                var dbContext = services.GetRequiredService<NoteTakerContext>();
+                await dbContext.Database.MigrateAsync();
+
                 DefaultDbInitialiser.Initialise(services);
             }
 
